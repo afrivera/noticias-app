acf1f8c7a4585e1c30c2ac1a0b1efec0e9e1af41
arivera <andresrfelip@hotmail.com>
2022-02-25 21:22:56 -0500

    Implementación de componentes para la app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoNoticias from "./components/ListadoNoticias";
+
 
 function App() {
+
+  // definir categoria y noticias 
+  const [categoria, setCategoria] = useState('');
+  const [noticias, setNoticias] = useState([])
+
+  // al detectar el cambio en categoria se ejecute el componente
+  useEffect( ()=> {
+
+    const consultarAPI = async()=> {
+      const url = `https://newsapi.org/v2/top-headlines?country=co&category=${ categoria }&apiKey=f8e177d38829459f915857912d5f3866`;
+
+      const respuesta = await fetch( url );
+      const noticias = await respuesta.json();
+
+      setNoticias(noticias.articles);
+    }
+
+    consultarAPI();
+  },[categoria])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header 
+        titulo = 'Buscador de Noticias'
+      />
+
+      <div className="container white">
+        <Formulario 
+          setCategoria = { setCategoria }
+        />
+
+        <ListadoNoticias
+          noticias={ noticias }
+        />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Formulario.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import useSelect from '../hooks/useSelect';
+import styles from './Formulario.module.css';
+
+
+const Formulario = ({ setCategoria }) => {
+
+    const OPCIONES = [
+        {value: 'general', label:'General'},
+        {value: 'business', label:'Negocios'},
+        {value: 'entertainment', label:'Entretenimiento'},
+        {value: 'health', label:'Salud'},
+        {value: 'science', label:'Ciencia'},
+        {value: 'sports', label:'Deportes'},
+        {value: 'technology', label:'Tecnología'}
+    ]
+
+    // usar customhook
+    const[ categoria, SelectNoticias ] = useSelect('general', OPCIONES);
+
+    // submit al form pasar categoria al app
+    const handleSubmit = e => {
+        e.preventDefault();
+        setCategoria( categoria );
+    }
+
+    return (
+        <div className={`${styles.buscador}row`}>
+            <div className='col s12 m8 offset-m2'>
+                <form
+                    onSubmit={ handleSubmit }
+                >
+                    <h2 className={styles.heading}>Encuentra Noticias por Categoría</h2>
+
+                    <SelectNoticias />
+                    <div className='input-field col s12'>
+                        <input 
+                            type='submit'
+                            className={`${styles['btn-block']} btn-large amber darken-2`}
+                            value='Buscar'
+                        />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default Formulario
